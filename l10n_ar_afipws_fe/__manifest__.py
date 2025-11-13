076fa2a37c3a67368b9137e8400c1fd76f720294
Ezequiel Ludueña <63431484+ezeluduena@users.noreply.github.com>
2025-11-13 18:15:54 -0300

    Bump version to 16.0.1.4.0

--- l10n_ar_afipws_fe/__manifest__.py
+++ l10n_ar_afipws_fe/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Factura Electrónica Argentina",
-    'version': '16.0.1.3.0',
+    'version': '16.0.1.4.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'A2 Systems,ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA)',
