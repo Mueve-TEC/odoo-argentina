5aa86fb1f64e2531349a7a010339c007ba5e8894
Ezequiel Ludueña <ezeluduena123@gmail.com>
2026-04-15 14:41:09 -0300

    [MIG] l10n_ar_inflation_adjustment: update post_init_hook signature to use 'env' in Odoo 18

--- mueve-modules/l10n_ar_inflation_adjustment/__init__.py
+++ mueve-modules/l10n_ar_inflation_adjustment/__init__.py
@@ -3,15 +3,11 @@ from . import models
 from . import wizards
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """
     Hook ejecutado después de la instalación del módulo.
     Asigna automáticamente el tag "No Monetaria" a las cuentas correspondientes.
     """
-    from odoo import api, SUPERUSER_ID
-    
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    
     # Asignar tag a cuentas de todas las compañías argentinas
     companies = env['res.company'].search([
         ('country_id.code', '=', 'AR')
