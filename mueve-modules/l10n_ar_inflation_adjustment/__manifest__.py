c3576f01af2ef7f26a95d64763c04e5ea088aa36
Ezequiel Ludueña <ezeluduena123@gmail.com>
2026-04-15 14:41:00 -0300

    [MIG] l10n_ar_inflation_adjustment: update manifest to 18.0.1.0.0

--- mueve-modules/l10n_ar_inflation_adjustment/__manifest__.py
+++ mueve-modules/l10n_ar_inflation_adjustment/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Mueve",
     'website': "https://github.com/Mueve-TEC/odoo-argentina",
     'category': 'Accounting',
-    'version': "16.0.1.0.0",
+    'version': "18.0.1.0.0",
     'license': 'AGPL-3',
     'depends': [
         'account',
